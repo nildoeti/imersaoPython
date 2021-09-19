b0b2e168944de12c3719fa5f6898b41184afde1a
Nildo <nildo@webstack.com.br>
2021-09-19 18:22:11 -0300

    Variaveis_e_atribuicoes.py adicioando ao projeto

---
+++ introducao/variaveis_e_atribuicoes.py
@@ -0,0 +1,9 @@
+'''
+    A forma padrão para a instrução de atribuição entre variáveis é:
+    <variavel> = <valor>
+    A declaracao valor que é atribuida em variavel, pode ser um valor booleano,
+    algébrico entre outras expressões como uma cadeia de caracteres.
+'''
+
+x = 3 # instrução de atribuição
+cor = 'azul' # expressões de atribuição do tipo cadeia de caracteres
