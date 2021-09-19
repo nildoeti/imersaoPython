8c8e228e0643078ac83e147023d626a51533f78f
Nildo <nildo@webstack.com.br>
2021-09-19 18:12:36 -0300

    operador_and_or_not.py atualizado

---
+++ introducao/operadores_and_or_not.py
@@ -0,0 +1,10 @@
+"""
+    Uso das expressões and, or e not em expressões boobleanas
+    IMPORTANTE
+    Operadores de comparação tem precedência sobre os operadores boobleanos
+    O operador not, é um operador unário
+"""
+
+print(2 < 3 and 4 > 5)  # False
+print(2 < 3 and True)  # True
+print(not (3 < 2))  # True
