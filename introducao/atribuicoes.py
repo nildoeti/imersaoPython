e8a4ae89b8468cb807ee961f9253f4f383f2a55b
Nildo <nildo@webstack.com.br>
2021-09-19 18:38:58 -0300

    adicioando atribuicoes.py ao projeto

---
+++ introducao/atribuicoes.py
@@ -0,0 +1,16 @@
+'''
+    Escreva instruções Python que correspondem às ações a seguir e execute-as:
+    (a) Atribua o valor inteiro 3 à variável a.
+    (b) Atribua 4 à variável b.
+    (c) Atribua à variável c o valor da expressão a * a + b * b.
+'''
+
+# (a) Atribua o valor inteiro 3 à variável a.
+a = 3
+
+# (b) Atribua 4 à variável b.
+b = 4
+
+# Atribua à variável c o valor da expressão a * a + b * b.
+c = a * a + b * b
+print(c)
