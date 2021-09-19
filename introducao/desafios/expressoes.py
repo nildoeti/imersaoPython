1ea1d3cdc8b2cde0d66439627b3126eb39374c26
Nildo <nildo@webstack.com.br>
2021-09-19 20:03:18 -0300

    expressoes.py atualizado.

---
+++ introducao/desafios/expressoes.py
@@ -0,0 +1,34 @@
+"""
+    Escreva expressões Python usando s1, s2 e s3 e os operadores + e * a fim de
+    avaliar para:
+    (a) 'ant bat cod'
+    (b) 'ant ant ant ant ant ant ant ant ant ant'
+    (c) 'ant bat bat cod cod cod'
+    (d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+"""
+# (a) 'ant bat cod'
+print("ant bar cod")
+
+# (b) 'ant ant ant ant ant ant ant ant ant ant'
+print("ant " * 10)
+
+# (c) 'ant bat bat cod cod cod'
+print("ant " + "bat " * 2 + "cod " * 3)
+
+# (d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+print(
+    "ant "
+    + "bat "
+    + "ant "
+    + "bat "
+    + "ant "
+    + "bat "
+    + "ant "
+    + "bat "
+    + "ant "
+    + "bat "
+    + "ant "
+    + "bat "
+    + "ant "
+    + "bat"
+)
