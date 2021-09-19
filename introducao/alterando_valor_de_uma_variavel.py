2654a281f74b3ad836ab301616cf7965dbfb6d21
Nildo <nildo@webstack.com.br>
2021-09-19 18:45:11 -0300

    alterando_valor_de_uma_variavel.py atualizado.

---
+++ introducao/alterando_valor_de_uma_variavel.py
@@ -0,0 +1,9 @@
+"""
+    supondo que a variável idade tenha seu valor pré-definido, o mesmo
+    é verdade que Python permite alterar seu valor ao longo de sua execução
+"""
+idade = 23
+print("Idade antes da alteração:", idade)
+
+idade = 20
+print("Idade depois da alteração:", idade)
