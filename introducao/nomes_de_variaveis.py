2ad62e1391072d7a362faf71c77f178d498a9829
Nildo <nildo@webstack.com.br>
2021-09-19 19:02:08 -0300

    Atualizado nomes_de_variaveis.py

---
+++ introducao/nomes_de_variaveis.py
@@ -0,0 +1,24 @@
+"""
+    A REGRA É CLARA:
+    nomes de variáveis podem ter letras minúsculas e maiúsculas(a-z, A-Z),
+    caractere de sublinhado(_), números desde que não seja o primeiro valor
+    do nome da variável
+
+    EXEMPLOS válidos
+    idade
+    ano_novo
+    saldoAtual
+
+    CARACTERES especiais
+    Desde a versão Python 3.0, foi adicionado a capacidade de criar variáveis
+    com o uso de caracteres especiais.
+    Isso mudança reflete a adaptação da linguagem na grande esfera que
+    compõe a globalização atual.
+
+    NÃO PODE
+    Usar palavras chaves da linguagem como variáveis como exemplo:
+    print, if, else, etc.
+"""
+país = "brasil"
+# variável com uso de acento
+print(país)
