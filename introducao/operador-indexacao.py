da2d30a3448102cfacc3de0a1a93e9e1bdc4c93e
Nildo <nildo@webstack.com.br>
2021-09-19 20:31:28 -0300

    operadores-indexacao.py atualizado.

---
+++ introducao/operador-indexacao.py
@@ -0,0 +1,13 @@
+"""
+    [] é um operador de indexação que server para acessar valores individuais
+    de uma string com base em seu índice que em computação é definido como:
+    0, para o primeiro valor do indice,
+    1, para o segundo valor do índice e assim por diante
+"""
+
+numeros = "123456789"
+
+print(numeros[0])  # retorna o valor para o indice 0
+print(numeros[1])  # retor o valor para o índice 1
+print(numeros[-1])  # retorna o valor para o ultimo indice
+print(numeros[-2]) # retorna o valor para o penultimo indice
