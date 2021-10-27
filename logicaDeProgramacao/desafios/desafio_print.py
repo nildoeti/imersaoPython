c3f460053cb3082723e41294d51722e985a65e41
Nildo <nildo@webstack.com.br>
2021-10-26 21:14:20 -0400

    desafios_print.py atualizado.

---
+++ logicaDeProgramacao/desafios/desafio_print.py
@@ -0,0 +1,6 @@
+"""
+    sep e end são argumentos da função print que representam respectivamente
+    separador e fim.
+"""
+print('Estou', 'aprendendo', 'Python', sep='-', end='')
+print('isso é muito', 'legal', sep='-', end='')
