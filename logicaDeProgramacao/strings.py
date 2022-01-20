ce9fcb0b3af2c5d45a9a09441f87f61304132da2
Nildo <nildo.webstack.com.br>
2022-01-20 10:54:40 -0500

    Gerado correção dos comentários

--- logicaDeProgramacao/strings.py
+++ logicaDeProgramacao/strings.py
@@ -1,11 +1,4 @@
-"""
-    Strings é um tipo de dado primitivo em Python
-    Que representa um texto delimitado entre aspas simples e/ou duplas.
 
-    OBS: Tudo em Python é um object
-
-    str - string
-"""
 
 print('Teste de exibição de strings delimitado entre "aspas simples"')
 print("Um texto delimitado entre 'aspas duplas'")
