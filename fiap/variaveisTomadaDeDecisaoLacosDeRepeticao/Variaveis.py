15896871449c5b0452d279f8665836cc9810df7e
Nildo <nildo@webstack.com.br>
2022-04-09 13:23:13 -0300

    finalizado exemplo para uso de variáveis em python

---
+++ fiap/variaveisTomadaDeDecisaoLacosDeRepeticao/Variaveis.py
@@ -0,0 +1,9 @@
+nome = 'Fulano de Tal'  # variável tipo string
+empresa = 'WebStack'  # variável tipo string
+qtd_functionarios = 500  # variável tipo inteiro
+mediaMensaliade = 800.90  # variável tipo float
+
+
+print(nome + ' trabalha na empresa ' + empresa)
+print('Possui ' + str(qtd_functionarios) + '  funcionários.')
+print('A média de mensalidades é de: ' + str(mediaMensaliade))
